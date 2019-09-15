404412f17db9e2c7a5092e788733af364ba8e706
joseteodoro <jteodoro.dev@gmail.com>
2019-09-15 13:36:37 -0300

    adding generated source code from schema

---
+++ cmd/server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/handler"
+	"github.com/joseteodoro/go-graphql-demo/api/auth"
+	"github.com/joseteodoro/go-graphql-demo/api/dal"
+	"github.com/joseteodoro/go-graphql-demo/api/dataloaders"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	db, err := dal.Connect()
+	checkErr(err)
+
+	initDB(db)
+
+	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	rootHandler := dataloaders.DataloaderMiddleware(
+		db,
+		handler.GraphQL(
+			go_graphql_demo.NewExecutableSchema(go_graphql_demo.NewRootResolvers(db)),
+			handler.ComplexityLimit(200)),
+	)
+	http.Handle("/query", auth.AuthMiddleware(rootHandler))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initDB(db *sql.DB) {
+	dal.MustExec(db, "DROP TABLE IF EXISTS reviews")
+	dal.MustExec(db, "DROP TABLE IF EXISTS screenshots")
+	dal.MustExec(db, "DROP TABLE IF EXISTS videos")
+	dal.MustExec(db, "DROP TABLE IF EXISTS users")
+	dal.MustExec(db, "CREATE TABLE public.users (id SERIAL PRIMARY KEY, name varchar(255), email varchar(255))")
+	dal.MustExec(db, "CREATE TABLE public.videos (id SERIAL PRIMARY KEY, name varchar(255), description varchar(255), url text,created_at TIMESTAMP, user_id int, FOREIGN KEY (user_id) REFERENCES users (id))")
+	dal.MustExec(db, "CREATE TABLE public.screenshots (id SERIAL PRIMARY KEY, video_id int, url text, FOREIGN KEY (video_id) REFERENCES videos (id))")
+	dal.MustExec(db, "CREATE TABLE public.reviews (id SERIAL PRIMARY KEY, video_id int,user_id int, description varchar(255), rating varchar(255), created_at TIMESTAMP, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (video_id) REFERENCES videos (id))")
+	dal.MustExec(db, "INSERT INTO users(name, email) VALUES('Ridham', 'contact@ridham.me')")
+	dal.MustExec(db, "INSERT INTO users(name, email) VALUES('Tushar', 'tushar@ridham.me')")
+	dal.MustExec(db, "INSERT INTO users(name, email) VALUES('Dipen', 'dipen@ridham.me')")
+	dal.MustExec(db, "INSERT INTO users(name, email) VALUES('Harsh', 'harsh@ridham.me')")
+	dal.MustExec(db, "INSERT INTO users(name, email) VALUES('Priyank', 'priyank@ridham.me')")
+}
---
+++ pkg/consent_manager/resolvers.go
@@ -0,0 +1,59 @@
+package consent_manager
+
+import (
+	"context"
+) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+type Resolver struct{}
+
+func (r *Resolver) Mutation() MutationResolver {
+	return &mutationResolver{r}
+}
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type mutationResolver struct{ *Resolver }
+
+func (r *mutationResolver) CreateDataType(ctx context.Context, display string) (*DataType, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) CreateGroup(ctx context.Context, display string) (*Group, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) CreatePatient(ctx context.Context, display string, groups []string, dataTypes []string) (*Patient, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) CreatePractioner(ctx context.Context, display string, groups []string) (*Practioner, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) CreateConsentEnforcementRule(ctx context.Context, display string, readerGroups []string, targetGroups []string, expiration int) (*ConsentEnforcement, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) CreateContract(ctx context.Context, display string, clauses []string) (*Contract, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) GiveConsent(ctx context.Context, patientID string, contractID string, expiration int) (*Signature, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) PatientResetPassphase(ctx context.Context, patientID string, restorePhrase string) (*Patient, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) PractionerResetPassphase(ctx context.Context, practionerID string, restorePhrase string) (*Practioner, error) {
+	panic("not implemented")
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) DataTypes(ctx context.Context, limit *int, offset *int) ([]*DataType, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) PublicGroups(ctx context.Context, display *string, limit *int, offset *int) ([]*Group, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Consent(ctx context.Context, patientID string, practionerID string, dataTypeID *string) (*AccessToken, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) ConsentEnforcement(ctx context.Context, patientID string, practionerID string, reason string) (*AccessToken, error) {
+	panic("not implemented")
+}
